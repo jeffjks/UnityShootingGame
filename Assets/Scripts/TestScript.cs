c0162159a656f958fa02022b5b403bf3152a23ea
KunSang.Cho <jeff_jks@naver.com>
2023-05-04 17:30:03 +0900

    폭발 이펙트 Json 사용 테스트

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,15 +1,36 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Text;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public struct JsonTest_Explosion {
+    ExplosionEffect explosionEffect;
+    ExplosionAudio explosionAudio;
+    Vector3 position;
+    float radius;
+    float[] speed;
+    float[] direction;
+    int duration;
+    int[] timer_add;
+    int number;
+}
+
 public class TestScript : MonoBehaviour
 {
-    public InputField m_InputField;
-
-    void Update()
+    void Start()
     {
-        Debug.Log(m_InputField.isFocused);
-        m_InputField.ActivateInputField();
+        List<JsonTest_Explosion> list = new List<JsonTest_Explosion>() {new JsonTest_Explosion(), new JsonTest_Explosion()};
+        string jsonString = JsonUtility.ToJson(list, true);
+        Save(jsonString);
+    }
+
+    public void Save(string jsonString)
+    {
+        FileStream fileStream = new FileStream(string.Format("{0}/{1}.json", Application.dataPath, "TestJsonData"), FileMode.Create);
+        byte[] data = Encoding.UTF8.GetBytes(jsonString);
+        fileStream.Write(data, 0, data.Length);
+        fileStream.Close();
     }
 }

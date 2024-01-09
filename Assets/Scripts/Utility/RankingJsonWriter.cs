86d5e05c579c0ebea113b7d09b6453e07cb3b8ea
KunSang.Cho <jeff_jks@naver.com>
2024-01-09 23:34:19 +0900

    Ranking 파일 경로 오류 수정

--- Assets/Scripts/Utility/RankingJsonWriter.cs
+++ Assets/Scripts/Utility/RankingJsonWriter.cs
@@ -15,6 +15,10 @@ public class RankingJsonWriter : MonoBehaviour
     {
         foreach (var fileName in _fileList)
         {
+            var directoryInfo = new DirectoryInfo(GameManager.RankingFilePath);
+            if (!directoryInfo.Exists)
+                directoryInfo.Create();
+            
             if (!File.Exists($"{GameManager.RankingFilePath}{fileName}"))
                 Utility.SaveDataFile(GameManager.RankingFilePath, fileName, _localRankingData);
         }
